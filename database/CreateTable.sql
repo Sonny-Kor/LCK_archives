8e45c9d48103d643d3c95f781106734e97464c84
SJ_Respect <46300191+Sonny-Kor@users.noreply.github.com>
2023-05-26 03:10:24 +0900

    [UPDATE]설계서 미완, 데이터베이스 user 권한 추가 Table Column 추가

--- database/CreateTable.sql
+++ database/CreateTable.sql
@@ -1,4 +1,5 @@
 
+
 CREATE DATABASE LCKArcive 
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
@@ -12,7 +13,9 @@ CREATE TABLE team (
   team_id INT PRIMARY KEY AUTO_INCREMENT,
   team_name VARCHAR(255) NOT NULL,
   team_logo VARCHAR(255)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET utf8 
+  DEFAULT COLLATE utf8_general_ci;
 
 -- 선수(Player) 테이블
 CREATE TABLE player (
@@ -20,19 +23,38 @@ CREATE TABLE player (
   player_name VARCHAR(255) NOT NULL,
   player_nickname VARCHAR(255) NOT NULL,
   player_img VARCHAR(255),
-  position VARCHAR(255),
+  player_position VARCHAR(255),
   team_id INT,
   FOREIGN KEY (team_id) REFERENCES team(team_id) ON DELETE SET NULL ON UPDATE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET utf8 
+  DEFAULT COLLATE utf8_general_ci;
 
--- 스케줄(match) 테이블
-CREATE TABLE `match` (
+-- 경기(match) 테이블
+CREATE TABLE _match (
   match_id INT PRIMARY KEY AUTO_INCREMENT,
   match_date DATETIME NOT NULL,
   team1_id INT,
   team1_score INT,
   team2_id INT,
   team2_score INT,
+  youtube_link VARCHAR(255),
+
   FOREIGN KEY (team1_id) REFERENCES team(team_id) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (team2_id) REFERENCES team(team_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET utf8 
+  DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE administer (
+  a_id INT PRIMARY KEY NOT NULL,
+  a_pw VARCHAR(255) NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET utf8 
+  DEFAULT COLLATE utf8_general_ci;
+
+DESC team;
+DESC player;
+DESC _match;
+
+DESC administer;
--- database/auth.sql
+++ database/auth.sql
@@ -1,1 +1,12 @@
+-- 비밀번호 변경법
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'workspace!';
+
+-- host user password 확인
+use mysql;
+select host, user, authentication_string from user;
+
+
+-- user 읽기 권한 설정
+CREATE USER 'new_user'@'localhost' IDENTIFIED BY 'workspace!';
+GRANT SELECT ON lckarcive.* TO 'new_user'@'localhost';
+FLUSH PRIVILEGES;
