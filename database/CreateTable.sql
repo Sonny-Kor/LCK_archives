4b7293f8caa808a0515079008288e62b85dfb158
SeungJae Son <aldladdl3@naver.com>
2023-05-25 23:28:23 +0900

    [UPDATE] 테이블명 변경, 데이터 삽입 SQL 추가 , 원시데이터 추가

--- database/CreateTable.sql
+++ database/CreateTable.sql
@@ -2,7 +2,11 @@
 CREATE DATABASE LCKArcive 
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
-  
+
+
+USE LCKArcive;
+
+
 -- 팀(Team) 테이블
 CREATE TABLE team (
   team_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -15,15 +19,15 @@ CREATE TABLE player (
   player_id INT PRIMARY KEY AUTO_INCREMENT,
   player_name VARCHAR(255) NOT NULL,
   player_nickname VARCHAR(255) NOT NULL,
-  img VARCHAR(255),
+  player_img VARCHAR(255),
   position VARCHAR(255),
   team_id INT,
   FOREIGN KEY (team_id) REFERENCES team(team_id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
--- 스케줄(Schedule) 테이블
-CREATE TABLE schedule (
-  schedule_id INT PRIMARY KEY AUTO_INCREMENT,
+-- 스케줄(match) 테이블
+CREATE TABLE `match` (
+  match_id INT PRIMARY KEY AUTO_INCREMENT,
   match_date DATETIME NOT NULL,
   team1_id INT,
   team1_score INT,
---
+++ database/InsertData.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO team (team_name, team_logo)
+VALUES
+('T1', 'T1'),
+('Gen.G eSports', 'Gen.G eSports'),
+('kt Rolster', 'kt Rolster'),
+('Dplus KIA', 'Dplus KIA'),
+('Hanwha Life Esports', 'Hanwha Life Esports'),
+('Nongshim RedForce', 'Nongshim RedForce'),
+('KWANGDONG FREECS', 'KWANGDONG FREECS'),
+('Liiv Sandbox', 'Liiv Sandbox'),
+('DRX', 'DRX'),
+('BRION', 'BRION');
